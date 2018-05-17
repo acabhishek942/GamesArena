92e44a68d44591cc1a4b2f11dcaa9ffe03cf094f
Abhishek Choudhary <acabhishek942@gmail.com>
2018-05-18 02:38:07 +0530

    started making games arena

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+    <p>{{ greeting() }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      title: 'Games-Arena'
+    }
+  },
+  methods:{
+    greeting: function(){
+      return 'Welcome to Game Arena';
+    }
+  }
+}
+</script>
+
+<style>
+</style>
