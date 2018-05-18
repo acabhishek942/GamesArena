1bad0097703a5a48e0c2b1f1d379f744d0c24b77
Abhishek Choudhary <acabhishek942@gmail.com>
2018-05-18 16:27:07 +0530

    Added header and footer components

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,24 @@
 <template>
   <div>
+    <app-header v-bind:title="title"></app-header>
     <list-games></list-games>
+    <app-footer v-bind:title="title"></app-footer>
   </div>
 </template>
 
 <script>
-import listGames from './components/listGames.vue'
+import listGames from './components/listGames.vue';
+import Header from './components/Header.vue';
+import Footer from './components/Footer.vue';
 export default {
   components:{
-    'list-games': listGames
+    'list-games': listGames,
+    'app-header': Header,
+    'app-footer': Footer,
   },
   data () {
     return {
+      title: 'Games Arena'
     }
   },
   methods:{
