5cb5b74fbf6a3a29a6d078c3cd3029e25c06d845
Abhishek Choudhary <acabhishek942@gmail.com>
2018-05-19 23:42:58 +0530

    Stick footer to the bottom using css

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -21,6 +21,11 @@ export default {
 footer{
     background: #222;
     padding: 6px;
+    position: fixed;
+    left: 0;
+    bottom: 0;
+    width: 100%;
+    text-align: center;
 }
 p{
         color: lightgreen;
